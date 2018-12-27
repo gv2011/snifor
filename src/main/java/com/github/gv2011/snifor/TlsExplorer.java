26f16c5812f650fa15e0f1e60dd3fdda6df9ce18
Vinz <gv2011@users.noreply.github.com>
2018-12-27 17:59:43 +0100

    license

--- src/main/java/com/github/gv2011/snifor/TlsExplorer.java
+++ src/main/java/com/github/gv2011/snifor/TlsExplorer.java
@@ -1,3 +1,33 @@
+/*-
+ * The MIT License (MIT)
+ *
+ * Modified version of tlschannel.impl.TlsExplorer
+ *
+ * Copyright (C) 2018 Vinz (https://github.com/gv2011)
+ *
+ * Original version from Mariano Barrios:
+ * https://github.com/marianobarrios/tls-channel/blob/5e3c04287eccf46ea365cc96264cd517ac7aea28/
+ * src/main/java/tlschannel/impl/TlsExplorer.java (MIT License)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
 package com.github.gv2011.snifor;
 
 import java.nio.BufferUnderflowException;
